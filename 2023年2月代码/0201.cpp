2a2344f14a9eb63a8446439acf95dbc2b49a82fd
Su-github-account <1247323390@qq.com>
2023-02-07 16:31:41 +0800

    从今天开始，提交代码

---
+++ 2023年2月代码/0201.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main(void)
+//{
+//	
+//	const char* pc = "hell bit";
+//	char arr[] = "hell bit";
+//
+//	printf("%s\n", pc);
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)								
+//{
+//	int n = 0, a = 0, sum = 0;
+//	scanf("%d%d", &n, &a);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int t = a;
+//		sum += a;
+//		a = a * 10 + t;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int mony = 0, empty = 0;
+//	scanf("%d", &mony);
+//	empty = mony;
+//	
+//	mony = empty / 2;
+//	mony+=
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	puts('a');
+//
+//	return 0;
+//}
