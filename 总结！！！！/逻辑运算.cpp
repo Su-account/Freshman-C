7bfd081606fc8119b34c5c3d2d0d87087dc314e8
Su-github-account <1247323390@qq.com>
2023-02-07 14:15:28 +0800

    测试

---
+++ 总结！！！！/逻辑运算.cpp
@@ -0,0 +1,14 @@
+//Âß¼­ÔËËã&&
+#include<stdio.h>
+
+int main(void)
+{
+	int i=0, a=0, b=2, c=3, d=4;
+	
+	i=a++ && ++b && d++;
+	printf("a=%d\nb=%d\nc=%d\nd=%d\n",a, b, c, d);
+	
+	a=1, b=2, c=3, d=4;
+	printf("a=%d\nb=%d\nc=%d\nd=%d\n",a, b, c, d);
+	return 0;
+ } 
