bb772d7ce44f20b38e82c2a9c442cf817c71514c
Su-github-account <1247323390@qq.com>
2023-02-19 10:42:41 +0800

    题单：顺序结构

---
+++ 洛谷0219.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//	printf("  %c\n %c%c%c\n%c%c%c%c%c\n", ch, ch, ch, ch, ch, ch, ch, ch, ch);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	long long count = 0, sum = 0;
+//	scanf("%lld%lld", &count, &sum);
+//	printf("%lld\n", count * sum);
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<ctype.h>
+//
+//int main(void)
+//{
+//	char ch = getchar();
+//	printf("%c\n", toupper(ch));
+//	
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	char a, b, c, d;
+//	scanf("%c%c%c.%c", &a, &b, &c, &d);
+//
+//	printf("%c.%c%c%c\n", d, c, b, a);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	double t = 0;
+//	int n = 0;
+//	scanf("%lf%d", &t, &n);
+//	printf("%.3lf\n%d\n", t / n, 2 * n);
+//	return 0;
+//}
