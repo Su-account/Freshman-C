edad4503051614a30944f7c1399779d57ed52330
Su-github-account <1247323390@qq.com>
2023-02-22 23:02:45 +0800

    郑州轻工业

---
+++ 0222.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main(void)
+//{
+//	int stay = 0;
+//	int sum = 0;
+//	scanf("%d", &stay);
+//	int floor[1000] = { 0 };
+//	int i = 0;
+//	for ( i = 0; i < stay; i++)
+//	{
+//		scanf("%d", &floor[i]);
+//	}
+//	sum += (stay) * 5;
+//	sum += (floor[0]) * 6;
+//	for (int j = 0; j < stay-1; j++)
+//	{
+//		if (floor[j] < floor[j + 1])
+//			sum += (floor[j + 1] - floor[j]) * 6;
+//		else if(floor[j]>floor[j+1])
+//			sum += (floor[j] - floor[j + 1]) * 4;
+//			
+//	}
+//	printf("%d\n", sum);
+//	
+//	return 0;
+//}
+
