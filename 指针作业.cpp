d3eb0cf3687dc3d4f3c436598eabfce15ad07858
Su-github-account <1247323390@qq.com>
2023-03-01 23:25:18 +0800

    指针练习

---
+++ 指针作业.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int a = 0, b = 0, t = 0;
+//	scanf("%d%d", &a, &b);
+//	if (a > b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int a = 0, b = 0;
+//	scanf("%d%d", &a, &b);
+//	if (a > b)
+//	{
+//		a = a ^ b;
+//		b = a ^ b;
+//		a = a ^ b;
+//	}
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int a = 0, b = 0, c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	int* m = &a;
+//	if (*m < b)
+//		m =& b;
+//	if (*m < c)
+//		m = &c;
+//	printf("%d\n", *m);
+//	return 0;
+//}
